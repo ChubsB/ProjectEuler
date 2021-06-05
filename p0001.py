1200f7e914deb2fef9e9a14083d1055ce50ac460
Hussain <hussainimam01@gmail.com>
2021-06-05 06:32:42 +0500

    Found a much better solution online thus requiring and update

--- p0001.py
+++ p0001.py
@@ -1,10 +1,17 @@
 #We need to calculate all multiples of 3 and 5 that are lesser than given number N
-
+#My initial attempt wasnt great so i took help from this wonderful article https://medium.com/@TheZaki/project-euler-1-multiples-of-3-and-5-c24cb64071b0
 N = 10
 
-def test(N):
-    total = sum(x for x in range(N) if (x % 3 == 0 or x % 5 == 0))
-    return total 
+def sum(n, k):
+    d = n // k
+    return k * (d * (d+1)) // 2
+
+def calculator(n):
+    return sum(n, 3) + sum(n, 5) - sum(n, 15)
+
+
+def test(n):
+     return sum(n, 3) + sum(n, 5) - sum(n, 15)
 
     
-print(test(N))
+print(test(N-1))
